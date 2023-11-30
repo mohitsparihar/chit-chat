ccd6778bc49884a7c1e1650a382443a324d1b0c3
Mohit Singh <mohitsinghparihar1511@gmail.com>
2023-11-30 21:32:28 +0530

    renamed the metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="mx-auto main-container">
+      <h1 className="text-4xl mb-7">Chit Chat</h1>
       <Link href="/login">
         <button className="bg-blue-500 text-white mt-2 p-2 rounded-md">
           Login
